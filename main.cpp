63695f73aa409001a977bc737e2b9e9d13be38e1
Yutaka Tsutano <yutaka@tsutano.com>
2017-05-02 23:26:19 -0500

    Use Boost.ProgramOptions to process arguments

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Yutaka Tsutano
+ * Copyright (c) 2016, 2017, Yutaka Tsutano
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -15,11 +15,18 @@
  */
 
 #include <iostream>
+#include <vector>
+#include <string>
 
 #include "jitana/util/axml_parser.hpp"
-#include <boost/property_tree/xml_parser.hpp>
 
-void process(const std::string& filename)
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+
+void process_xml(std::ostream& os, const std::string& filename)
 {
     boost::property_tree::ptree pt;
     try {
@@ -28,32 +35,57 @@ void process(const std::string& filename)
     }
     catch (const jitana::axml_parser_magic_mismatched& e) {
         // Binary parser has faied: try to read it as a normal XML file.
-        boost::property_tree::read_xml(
-                filename, pt, boost::property_tree::xml_parser::trim_whitespace,
-                std::locale());
+        boost::property_tree::read_xml(filename, pt);
     }
 
-    write_xml(std::cout, pt);
-    std::cout << "\n";
+    boost::property_tree::xml_writer_settings<std::string> settings(' ', 2);
+    write_xml(os, pt, settings);
 }
 
 int main(int argc, char** argv)
 {
-    if (argc <= 1) {
-        std::cerr << "usage: " << argv[0] << " input_file\n";
-        exit(1);
-    }
+    namespace po = boost::program_options;
+
+    // Declare command line argument options.
+    po::options_description desc("Allowed options");
+    desc.add_options()("help", "Display available options")(
+            "input-file,i", po::value<std::string>(), "Input file")(
+            "output-file,o", po::value<std::string>(), "Output file");
+    po::positional_options_description p;
+    p.add("input-file", -1);
 
     try {
-        process(argv[1]);
+        // Process the command line arguments.
+        po::variables_map vmap;
+        po::store(po::command_line_parser(argc, argv)
+                          .options(desc)
+                          .positional(p)
+                          .run(),
+                  vmap);
+        po::notify(vmap);
+
+        if (vmap.count("help") || !vmap.count("input-file")) {
+            // Print help and quit.
+            std::cout << "Usage: axmldec [options] <input_file>\n";
+            std::cout << desc << "\n";
+            return 0;
+        }
+
+        if (vmap.count("input-file")) {
+            // Construct the output stream.
+            std::ostream* os = &std::cout;
+            std::ofstream ofs;
+            if (vmap.count("output-file")) {
+                ofs.open(vmap["output-file"].as<std::string>());
+                os = &ofs;
+            }
+
+            // Process the file.
+            process_xml(*os, vmap["input-file"].as<std::string>());
+        }
     }
-    catch (const jitana::axml_parser_error& e) {
-        std::cerr << "binary parser failed: " << e.what() << "\n";
-    }
-    catch (const boost::property_tree::xml_parser::xml_parser_error& e) {
-        std::cerr << "XML parser failed: " << e.what() << "\n";
-    }
-    catch (const std::runtime_error& e) {
+    catch (std::exception& e) {
         std::cerr << "error: " << e.what() << "\n";
+        return 1;
     }
 }
